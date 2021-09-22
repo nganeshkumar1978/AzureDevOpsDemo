a42933de1c6504a2ee678a4850160fa82871188d
Ganesh kumar <nganeshkumar1978@gmail.com>
2021-09-22 22:27:15 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AzureDevOpsDemo.Models;
+using AzureDevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IPostRepository postRepository;
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
